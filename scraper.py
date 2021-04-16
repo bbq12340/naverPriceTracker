4353eb11e104ca185589daf24cdd23b35aee1996
bbq12340 <bbq12340@hotmail.com>
2021-04-16 14:38:39 +0900

    need fix on error handling

--- scraper.py
+++ scraper.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from retry import retry
+
 import json
+from json import JSONDecodeError
+import logging
+import random
+
 
 NAVER = "https://search.shopping.naver.com/search/all"
 
@@ -8,6 +14,8 @@ headers = {
     "referer": "https://shopping.naver.com/",
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
 }
+
+logging.basicConfig()
 
 
 def extract_product_list(query, p):
@@ -25,18 +33,34 @@ def extract_product_list(query, p):
     r = requests.get(NAVER, params=payload, headers=headers)
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, 'html.parser')
-        rel_links = [a['href'] for a in soup.find_all(
-            "a", {"class": "basicList_compare__3AjuT"})]
+        # rel_links = [a['href'] for a in soup.find_all(
+        #     "a", {"class": "basicList_compare__3AjuT"})]
+        data = soup.find('script', {'id': '__NEXT_DATA__'}).string
+        rel_links = [item['item']['crUrl'] for item in json.loads(
+            data)['props']['pageProps']['initialState']['products']['list']]
         return rel_links
 
 
-def search_store(stores, rel_links):
-    link = rel_links[0]
-    r = requests.get(link, headers=headers)
-    malls = r.url.split("?")[0]+"/malls?pr=PC"
-    response = requests.get(malls, headers={
+@retry(JSONDecodeError, delay=random.uniform(0, 5), jitter=1)
+def search_catalog(r, catalog):
+    res = requests.get(catalog, headers={
         'authority': 'search.shopping.naver.com',
         'referer': r.url,
         'urlprefix': '/api',
         'user-agent': headers['user-agent']
-    }).json()
+    }).json()['result']
+    malls = [mall['mallName'] for mall in res]
+    return malls
+
+
+def search_store(stores, rel_links):
+    result = []
+    for link in rel_links:
+        r = requests.get(link, headers=headers)
+        catalog = r.url.split("?")[0]+"/malls?pr=PC"
+        malls = search_catalog(r, catalog)
+        for store in stores:
+            if store in malls:
+                result.append(link)
+                break
+    return result
--- worker.py
+++ worker.py
@@ -1,4 +1,9 @@
 from scraper import extract_product_list, search_store
 
+targets = ['우아킹', '멸치쇼핑', '싸자나']
+
 rel_links = extract_product_list("런닝", 1)
-search_store(rel_links)
+result = search_store(targets, rel_links)
+for r in result:
+    with open('result.txt', 'a', encoding='utf-8-sig') as f:
+        f.write(r+"\n")
